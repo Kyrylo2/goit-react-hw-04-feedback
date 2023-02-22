ed5f2440137e4278c8fbd9e79dcff3efe17abf6a
Kyrylo <savchenko.kyrylo@gmail.com>
2023-02-22 17:34:11 +0200

    upd app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,8 @@
+import React from 'react';
 import Feedback from '../components/Feedback';
 
-import React from 'react';
-
-class App extends React.Component {
-  static defaultProps = {
-    good: 0,
-    bad: 0,
-    neutral: 0,
-  };
-
-  render() {
-    return <Feedback props={this.props} />;
-  }
-}
+const App = ({ good = 0, bad = 0, neutral = 0 }) => {
+  return <Feedback props={{ good, bad, neutral }} />;
+};
 
 export default App;
